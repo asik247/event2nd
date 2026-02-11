14acdc43b8d539af9c704732d5c1076dbdc18bc0
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-11 23:22:22 +0600

    event delegation

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 console.log("event 2nd code start now");
+
+// enent delegation code here now;
+// let orderList = document.getElementById("orderList");
+// orderList.addEventListener("click",(e)=>{
+//     if(e.target.tagName ==="LIST"){
+//         console.log(e.target);
+//     }
+// })
+let ol = document.getElementById("orderList2")
+ol.addEventListener("click",(e)=>{
+    // console.log("hi");
+
+
+
+    
+    // if(e.target.classList.contains("delete")){
+    //     e.target.parentNode.removeChild(e.target)
+    // }
+
+
+
+
+
+
+
+
+    // e.target.parentNode.removeChild(e.target)
+    // Remove items code here now;
+
+
+
+    // Rong way event delegation:-
+    // let dele = document.querySelectorAll(".delete");
+    // dele.forEach((del)=>{
+    //     // console.log(del);
+    //     del.addEventListener("click",(e)=>{
+    //         e.target.parentNode.removeChild(e.target)
+    //     })
+    // })
+})
